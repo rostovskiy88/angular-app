bef2143805343d06a5303631f433852a3085fc0c
Oleg Rostovskyi <rostovskiyoleg88@gmail.com>
2023-08-22 23:04:05 +0300

    add two-way binding, remove warning components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,11 +4,32 @@ import { Component } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: 'servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewSever = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'Test server';
+  username = '';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewSever = true
+    }, 2000);
+  }
+
+  onCreateSever() {
+    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
 
 }
